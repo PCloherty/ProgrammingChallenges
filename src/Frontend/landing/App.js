0498db8a40cc78e8d4297dfd4c08acdb13de963a
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2019-09-25 15:58:47 +0100

    initial set up for the repository

---
+++ src/Frontend/landing/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Hard from "../../Frontend/1-10/challenge1/hard.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Hard></Hard>
+    </div>
+  );
+}
+
+export default App;
