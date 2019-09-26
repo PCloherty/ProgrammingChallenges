dc92aa0519845b9ac2b602a539c61762b082f93d
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2019-09-26 20:21:21 +0100

    tictactoe self explanitory

--- src/Frontend/landing/App.js
+++ src/Frontend/landing/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Easy from "../1-10/challenge1/Easy.js"
 import Hard from "../../Frontend/1-10/challenge1/hard.js";
+import TicTacToe from "../1-10/tictactoe.js"
 
 function App() {
   return (
     <div className="App">
+      <TicTacToe></TicTacToe>
       <Hard></Hard>
+      <Easy></Easy>
     </div>
   );
 }
