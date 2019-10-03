d354b20562188cac9b4bef1db0ab357e1a71bb35
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2019-10-03 16:16:57 +0100

    file name changes

---
+++ src/Frontend/1-10/1-10.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Contact.css";
+
+export default function HomeContent() {
+    return (
+        <header3>Challenges 1-10</header3>
+        <div>Challenge 1</div>  
+        <div>Challenge 2</div>
+        <div>Challenge 3</div>
+        <div>Challenge 4</div>
+        <div>Challenge 5</div>
+        <div>Challenge 6</div>
+        <div>Challenge 7</div>
+        <div>Challenge 8</div>
+        <div>Challenge 9</div>
+        <div>Challenge 10</div>                       
+
+    )
--- src/Frontend/landing/App.js
+++ src/Frontend/landing/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import Easy from "../1-10/challenge1/Easy.js"
-import Hard from "../../Frontend/1-10/challenge1/hard.js";
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Easy from "../1-10/1/Easy.js.js"
+import Hard from "../1-10/1/hard.js.js";
 import TicTacToe from "../1-10/tictactoe.js"
 
 function App() {
   return (
     <div className="App">
-      <TicTacToe></TicTacToe>
-      <Hard></Hard>
-      <Easy></Easy>
+    <div id="content">
+      <Route exact path = "/" component={HomeContent}/>
+      <Route exact path = "/1-10" component={Projects}/>
+    </div>
+      
+     
     </div>
   );
 }
